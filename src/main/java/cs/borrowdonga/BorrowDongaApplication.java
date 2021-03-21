f8eeed6f23fd5d0f46b9c5a824b762a2ed9051c6
JinseongHwang <eddy5360@naver.com>
2021-03-21 21:01:39 +0900

    chore: 프로젝트 생성 (#3)
    * docs: Update README.md
    
    * chore: 스프링 프로젝트 생성

---
+++ src/main/java/cs/borrowdonga/BorrowDongaApplication.java
@@ -0,0 +1,13 @@
+package cs.borrowdonga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorrowDongaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorrowDongaApplication.class, args);
+	}
+
+}
