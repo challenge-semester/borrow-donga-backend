49f2405c678cb3d67d11616adb4a98833235a541
JinseongHwang <eddy5360@naver.com>
2021-03-21 13:20:13 +0900

    chore: 스프링 프로젝트 생성

---
+++ src/test/java/cs/borrowdonga/BorrowDongaApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.borrowdonga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorrowDongaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
