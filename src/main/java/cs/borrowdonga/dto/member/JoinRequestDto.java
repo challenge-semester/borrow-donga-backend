39c00d14292c10779a07032107280525277c3cf6
JinseongHwang <eddy5360@naver.com>
2021-05-26 18:13:47 +0900

    feat: DTO 객체를 Member 로 변경하는 메서드 추가

--- src/main/java/cs/borrowdonga/dto/member/JoinRequestDto.java
+++ src/main/java/cs/borrowdonga/dto/member/JoinRequestDto.java
@@ -1,12 +1,16 @@
 package cs.borrowdonga.dto.member;
 
+import cs.borrowdonga.domain.member.Member;
+import cs.borrowdonga.domain.member.MemberRole;
 import lombok.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
 public class JoinRequestDto {
 
-    private String name;
+    private String username;
     private String department;
     private String studentNumber;
     private String password;
@@ -14,12 +18,24 @@ public class JoinRequestDto {
     private String phoneNumber;
 
     @Builder
-    public JoinRequestDto(String name, String department, String studentNumber, String password, String email, String phoneNumber) {
-        this.name = name;
+    public JoinRequestDto(String username, String department, String studentNumber, String password, String email, String phoneNumber) {
+        this.username = username;
         this.department = department;
         this.studentNumber = studentNumber;
         this.password = password;
         this.email = email;
         this.phoneNumber = phoneNumber;
+    }
+
+    public Member toEntity() {
+        return Member.builder()
+                .password(new BCryptPasswordEncoder().encode(password))
+                .username(username)
+                .email(email)
+                .phoneNumber(phoneNumber)
+                .department(department)
+                .studentNumber(studentNumber)
+                .role(MemberRole.USER)
+                .build();
     }
 }
